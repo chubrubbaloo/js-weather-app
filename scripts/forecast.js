7796f6e1e2fe24503b658b76ec043f20c3251e0e
Haris Avdicevic <haris.avdi93@gmail.com>
2023-05-14 19:08:02 +0200

    implemented DOM manipulation for the search form

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,8 +1,7 @@
 // API key
 const key = 'g13GYf3QSsMnVe7lm1w5ZHS5FCeuHt7e';
 
-// get weather information
-
+// GET weather information
 const getWeather = async (id) => {
 
   const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
@@ -14,36 +13,22 @@ const getWeather = async (id) => {
   return data[0];
 };
 
-getWeather('308409');
 
-// get city information
-
+// GET city information
 const getCity = async (city) => {
 
-    // Base URL & the required query parameters for the API key and the city to search for.
     const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
     const query = `?apikey=${key}&q=${city}`;
 
-
-
-    // Concatenate the base and query variables and fetch data.
     const response = await fetch(base + query);
-
     const data = await response.json();
 
     return data[0];
-
 };
 
 "308409";
 
-getCity('Växjö')
-.then(data => {
-    return getWeather(data.Key);
-}).then(data => console.log(data))
-.catch(error => console.log(error.message));
 
-// getWeather('308409');
 
 
 
