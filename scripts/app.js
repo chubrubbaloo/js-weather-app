cd93140e31b5997f079faf1e7fb6c6952ff69570
Haris Avdicevic <haris.avdi93@gmail.com>
2023-05-15 12:56:37 +0200

    implemented function for displaying weather info in the UI

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,25 @@
 const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+
+const updateUI = (data) => {
+    const cityDetails = data.cityDetails;
+    const weather = data.weather;
+
+    // update details template
+    details.innerHTML = `
+    <h5 class="my-3">${cityDetails.EnglishName}</h5>
+    <div class="my-3">${weather.WeatherText}</div>
+    <div class="display-4 my-4">
+      <span>temp</span>
+      <span>${weather.Temperature.Metric.UnitType}C</span>
+    </div>
+    `;
+
+    if(card.classList.contains('d-none')){
+        card.classList.remove('d-none');
+    }
+};
 
 const updateCity = async (city) => {
 
@@ -15,7 +36,7 @@ const updateCity = async (city) => {
 };
 
 cityForm.addEventListener('submit', e => {
-    
+
     e.preventDefault();
 
     // get city value
@@ -24,7 +45,7 @@ cityForm.addEventListener('submit', e => {
 
     // update the UI with the new city
     updateCity(city)
-    .then(data => console.log(data))
-    .catch(error => console.log(error.message));
+        .then(data =>  updateUI(data))
+        .catch(error => console.log(error.message));
 
 });
